25babfbcdd052c1eb2e969a022f28640a6f4a337
LuisMPS <luismario14@gmail.com>
2021-04-27 12:58:31 -0500

    Fixed index ignore at position 0

--- packages/build-ui/src/reducers/tree/index/removeListIndex.js
+++ packages/build-ui/src/reducers/tree/index/removeListIndex.js
@@ -1,7 +1,9 @@
+import { original } from "immer";
+
 function removeListIndex(state, action) {
     const {id, name} = action.payload;
     const list = state.index_list[name];
-    const position = (list && list.indexOf(id)) || -1;
+    const position = list ? list.indexOf(id) : -1;
     const isIndex = position >= 0;
     // State mutations
     if (isIndex) state.index_list[name].splice(
--- packages/build-ui/src/reducers/tree/index/toggleListIndex.js
+++ packages/build-ui/src/reducers/tree/index/toggleListIndex.js
@@ -1,7 +1,7 @@
 function toggleListIndex(state, action) {
     const {id, name} = action.payload;
     const list = state.index_list[name];
-    const position = (list && list.indexOf(id)) || -1;
+    const position = list ? list.indexOf(id) : -1;
     const isIndex = position >= 0;
     // State mutations
     if (isIndex) state.index_list[name].splice(
