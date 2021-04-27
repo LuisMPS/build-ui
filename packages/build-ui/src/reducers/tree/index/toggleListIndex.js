c36eb71417048d3d85d53e8f2f749650753915a1
LuisMPS <luismario14@gmail.com>
2021-04-27 13:08:28 -0500

    Fixed toggling list index when it did not exist before

--- packages/build-ui/src/reducers/tree/index/toggleListIndex.js
+++ packages/build-ui/src/reducers/tree/index/toggleListIndex.js
@@ -4,10 +4,13 @@ function toggleListIndex(state, action) {
     const position = list ? list.indexOf(id) : -1;
     const isIndex = position >= 0;
     // State mutations
-    if (isIndex) state.index_list[name].splice(
-        position, 1
-    );
-    if (!isIndex) state.index_list[name].push(id);
+    if (isIndex) {
+        state.index_list[name].splice(position, 1);
+    }
+    if (!isIndex) {
+        state.index_list[name] = state.index_list[name] || [];
+        state.index_list[name].push(id);
+    }
 }
 
 const reducer = {
