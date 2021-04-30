7857360ae6e439fac9e371142803bf45a3f82633
LuisMPS <luismario14@gmail.com>
2021-04-30 17:28:40 -0500

    Moved unrecorded actions to extraActions instead of actions

--- packages/build-ui/src/history/wrappers/createVersionedSlice.js
+++ packages/build-ui/src/history/wrappers/createVersionedSlice.js
@@ -25,6 +25,7 @@ const createVersionedSlice = ({
     reducers,
     extraReducers,
 }) => {
+
     const history = {
         version: 0,
         present: initialState,
@@ -47,41 +48,63 @@ const createVersionedSlice = ({
         batch: batch,
         configuration: configuration,
     };
+
     const completeReducers = Object.entries(reducers)
     .reduce((completeReducers, [reducer_name, reducer]) => {
+        function withEnhancers(reducer, ...enhancers) {
+            const reducerFn = isFunction(reducer) ? reducer: reducer.reducer;
+            const reducerEnhanced = enhancers.reduce((reducer, enhancer) => (
+                enhancer(reducer)
+            ), reducerFn);
+            return (
+                isFunction(reducer) 
+                ? reducerEnhanced 
+                : {...reducer, reducer: reducerEnhanced}
+            );
+        }
         const batched_name = `${reducer_name}/batched`;
         const unbatched_name = `${reducer_name}/unbatched`;
-        completeReducers[batched_name] = isFunction(reducer) 
-            ? batched(reducer)
-            : {...reducer, reducer: batched(reducer.reducer)}
-        completeReducers[unbatched_name] = isFunction(reducer) 
-            ? unbatched(reducer)
-            : {...reducer, reducer: unbatched(reducer.reducer)}
-        completeReducers[reducer_name] = isFunction(reducer) 
-            ? signer(proxy(reducer))
-            : {...reducer, reducer: signer(proxy(reducer.reducer))}
+        const unrecorder_name = `${reducer_name}/unrecorded`;
+        completeReducers[batched_name] = withEnhancers(reducer, batched);
+        completeReducers[unbatched_name] = withEnhancers(reducer, unbatched);
+        completeReducers[unrecorder_name] = withEnhancers(reducer, proxy, signer);
+        // Raw reducer, as it would have 
+        // been returned by normal
+        // createSlice function. Behaviour
+        // is implemeted because this function
+        // is supposed to be a wrapper, so 
+        // there are should be no changes to
+        // root createSlice function in params
+        // or return values.
+        completeReducers[reducer_name] = reducer;
         return completeReducers;
     }, {});
+
     const signedHistoryReducers = Object.entries(historyReducers)
     .reduce((reducers, [type, reducer]) => {
         reducers[type] = signer(reducer);
         return reducers;
     }, {});
+
     const versionedReducers = {
         ...completeReducers,
         ...commitReducers,
         ...signedHistoryReducers,
     };
+
     const slice = createSlice({
         name: name,
         initialState: versionedState,
         reducers: versionedReducers,
         extraReducers: extraReducers,
     });
+
     const initialExtra = {
         batched: {},
         unbatched: {},
+        unrecorded: {},
     };
+
     const extraActions = Object.entries(slice.actions)
     .reduce((extraActions, [actionType, action]) => {
         const typeSplit = actionType.split('/').reverse();
@@ -91,8 +114,8 @@ const createVersionedSlice = ({
         }
         return extraActions;
     }, initialExtra);
-
     slice.extraActions = extraActions;
+    
     return slice;
 }
 
