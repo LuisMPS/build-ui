e24b013dd06496ef760dbd863b157f2be74f7bf9
LuisMPS <luismario14@gmail.com>
2021-05-31 10:55:47 -0500

    Added number isNumber and parseNumber functions

--- packages/build-ui/src/utils/number.js
+++ packages/build-ui/src/utils/number.js
@@ -1,2 +1,8 @@
 export const isInteger = obj =>
     String(Math.floor(Number(obj))) === obj;
+
+export const isNumber = obj => 
+    !Number.isNaN(Number.parseFloat(obj));
+
+export const parseNumber = obj => 
+    Number.parseFloat(obj);
