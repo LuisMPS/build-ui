314c46e8929a748f458b88e1bb0e660d1bc9392a
LuisMPS <luismario14@gmail.com>
2021-05-31 11:04:04 -0500

    Aborted timer on tree loading

--- packages/build-ui/src/hooks/useBuilder.js
+++ packages/build-ui/src/hooks/useBuilder.js
@@ -1,8 +1,7 @@
-import {useDispatch} from "react-redux";
 import {useReplacer} from "./useComposed";
+import useSketchContext from "./useSketchContext";
 import useVersion from "./useVersion";
 import {getTreeFromHistory} from "../selectors/tree";
-import {restartHistory} from "../slices/tree";
 
 const useBuilder = () => {
     const version = useVersion();
@@ -10,16 +9,13 @@ const useBuilder = () => {
     const tree = getTreeFromHistory(
         version.history
     );
-    const dispatch = useDispatch();
+    const context = useSketchContext();
+    const batcher = context.batcher;
     function json() {
         return tree;
     }
     function loadTree(tree) {
-        // Restart history before
-        // replacing tree since
-        // tree replacement is not
-        // recorded in history.
-        dispatch(restartHistory())
+        batcher.abort();
         // Tree should include
         // tree meta that will
         // be loaded.
