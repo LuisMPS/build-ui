5dc6c92f9f58289326d201f93aa1019cf7ed1b53
LuisMPS <luismario14@gmail.com>
2021-05-31 10:47:59 -0500

    Removed deprecated comment

--- packages/build-ui/src/history/enhancers/signer.js
+++ packages/build-ui/src/history/enhancers/signer.js
@@ -2,9 +2,10 @@ import commitHistory, {creator} from "../reducers/commitHistory"
 
 function signer(reducer) {
     return function(state, action) {
+        const commit = creator();
         // State mutations
-        // Commit (sign) any batched actions.
-        const commit = creator({});
+        // Commit (sign) any batched 
+        // actions.
         commitHistory(state, commit);
         reducer(state, action);
     }
