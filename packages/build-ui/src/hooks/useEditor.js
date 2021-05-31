6f2faa47bc1244fb4714176f35f410e28b0c5230
LuisMPS <luismario14@gmail.com>
2021-05-31 11:02:46 -0500

    Added meta update support and input value casting support

--- packages/build-ui/src/hooks/useEditor.js
+++ packages/build-ui/src/hooks/useEditor.js
@@ -1,12 +1,14 @@
 import useNodeDnD from "./dnd/useNodeDnD";
 import useCollector from "./collectors/useCollector";
 import useActions from "./useActions";
-import {setIn} from "../utils/object";
+import {getIn, setIn} from "../utils/object";
+import {isNumber, parseNumber} from "../utils/number";
+import {isFunction} from "../history/utils/function";
 
 const useEditor = ({
     id,
-    initialOnDrop,
-    initialOnDropDone,
+    onDrop,
+    onDropDone,
 }) => {
     const nodeSelector = selectors => (
         selectors.selectById(id)
@@ -18,22 +20,60 @@ const useEditor = ({
         collected.node && 
         collected.node.props
     );
+    const meta = (
+        collected.meta
+    );
     const dnd = useNodeDnD({
         initialTransferType: 'builder',
-        initialOnDrop: initialOnDrop,
-        initialOnDropDone: initialOnDropDone,
+        onDrop: onDrop,
+        onDropDone: onDropDone,
         id: id,
     });
     const actions = useActions();
+    function getEventValue(event) {
+        const target = event.target;
+        const type = target.type;
+        const value = target.value;
+        const checked = target.checked;
+        switch (type) {
+            case 'checkbox':
+                return checked;
+            case 'number':
+            case 'range': 
+                const number = parseNumber(value); 
+                return isNumber(number) ? number : value;
+            default:
+                return value;
+        }
+    }
     function handleUpdate(event, parser) {
         const name = event.target.name;
-        const value = parser 
-        ? parser(event.target.value)
-        : event.target.value;
-        const props = setIn({}, name, value);
+        const value = getEventValue(event);
+        const previousValue = getIn(props, name);
+        const parsedValue = (
+            parser && isFunction(parser) 
+            ? parser(value, previousValue)
+            : value
+        );
+        const props = setIn({}, name, parsedValue);
         actions.timeBatched.triggerUpdate({
             id: id,
             props: props,
+        });
+    }
+    function handleMetaUpdate(event, parser) {
+        const name = event.target.name;
+        const value = getEventValue(event);
+        const previousValue = getIn(meta, name);
+        const parsedValue = (
+            parser && isFunction(parser)
+            ? parser(value, previousValue)
+            : value
+        );
+        const props = setIn({}, name, parsedValue);
+        actions.timeBatched.triggerMetaUpdate({
+            id: id,
+            meta: props,
         });
     }
     function handlePanel(event) {
@@ -46,6 +86,7 @@ const useEditor = ({
     }
     const editorBag = {
         props: props,
+        meta: meta,
     }
     const {
         isTransfering,
@@ -56,6 +97,7 @@ const useEditor = ({
     } = dnd;
     const handlers = {
         handleUpdate,
+        handleMetaUpdate,
         handlePanel,
     }
     const flags = {
