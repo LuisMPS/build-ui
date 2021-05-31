58c174a8a8d5cb73e0882a24147b0e843afa6708
LuisMPS <luismario14@gmail.com>
2021-05-31 11:14:22 -0500

    Exposed correct interface

--- packages/build-ui/src/index.js
+++ packages/build-ui/src/index.js
@@ -1,38 +1,43 @@
 import Builder from './components/Builder'
 import Workspace from './components/Workspace'
-import ChildrenDropListener from './components/ChildrenDropListener'
-import ChildrenDropBuilder from './components/ChildrenDropBuilder'
+import Node from './components/Node'
 import Panel from './components/Panel'
+import ViewProvider from './components/View'
 
-import DnDBuilder from './components/DnDBuilder';
-import DnDListener from './components/DnDListener';
+import DnDBuilder from './components/DnDBuilder'
+import DnDListener from './components/DnDListener'
+
+import DnDBuilderHOC from './hocs/DnDBuilderHOC'
 
 import useEditor from './hooks/useEditor'
 import useActions from './hooks/useActions'
 import useTools from './hooks/useTools'
 import useBuilder from './hooks/useBuilder'
+import useView from './hooks/useView'
 
 import useCollector from './hooks/collectors/useCollector'
 import useMultiCollector from './hooks/collectors/useMultiCollector'
 
-import useDnDHelpers from './hooks/dnd/useDnDHelpers';
-import useNodeDnD from './hooks/dnd/useNodeDnD';
-import useToolDnD from './hooks/dnd/useToolDnD';
+import useDnDHelpers from './hooks/dnd/useDnDHelpers'
+import useNodeDnD from './hooks/dnd/useNodeDnD'
+import useToolDnD from './hooks/dnd/useToolDnD'
 
 import {branch, item, itemid} from './utils/tree'
 
 export {
     Builder,
     Workspace,
-    ChildrenDropListener,
-    ChildrenDropBuilder,
+    Node,
     Panel,
+    ViewProvider,
     DnDBuilder,
     DnDListener,
+    DnDBuilderHOC,
     useEditor,
     useActions,
     useTools,
     useBuilder,
+    useView,
     useCollector,
     useMultiCollector,
     useDnDHelpers,
