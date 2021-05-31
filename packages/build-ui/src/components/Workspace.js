89ae2d443a7c6f1766135292b82347c05035a071
LuisMPS <luismario14@gmail.com>
2021-05-31 10:36:54 -0500

    Wrap in ViewProvider. Collect root id and pass to Node

--- packages/build-ui/src/components/Workspace.js
+++ packages/build-ui/src/components/Workspace.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import useCollector from '../hooks/collectors/useCollector';
+import ViewProvider from './View';
 import Node from "./Node"
 
 /*
@@ -8,10 +10,21 @@ import Node from "./Node"
 const Workspace = ({
     view,
 }) => {
-    return <Node
-        root = {true}
-        view = {view}
-    />
+    const selector = selectors => (
+        selectors.selectRoot()
+    );
+    const collected = useCollector({
+        selector: selector
+    });
+    const node = collected.node;
+    return node 
+    ? <ViewProvider view = {view}>
+        <Node
+            id = {node.id}
+            view = {view}
+        />
+    </ViewProvider>
+    : null;
 }
 
 export default Workspace;
