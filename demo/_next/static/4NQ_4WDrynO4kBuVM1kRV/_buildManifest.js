0626405851ec957ca864e0823c8d201816d89da2
LuisMPS <luismario14@gmail.com>
2021-06-02 15:55:18 -0500

    Updated demo with latest project version

---
+++ demo/_next/static/4NQ_4WDrynO4kBuVM1kRV/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-c7d2946dcd7234630ab7.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-070f4db04a49aaf85440.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
