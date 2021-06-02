f548189f164db198855e4b918d317469cfc4ce3f
LuisMPS <luismario14@gmail.com>
2021-06-02 08:58:28 -0500

    Fixed JSS warning

--- examples/demo/src/components/builder/ui/grid/style/Grid.js
+++ examples/demo/src/components/builder/ui/grid/style/Grid.js
@@ -1,23 +1,23 @@
 import {makeStyles} from '@material-ui/core/styles'
 import combineStyles from '../../../styles';
 
-const gridStyles = theme => ({
-    grid: () => {
-        const generateBreakpointStyle = size => ({
-            [theme.breakpoints.up(size)]: ({
-                width: props => props[size] && props[size].width,
-                height: props => props[size] && props[size].height,
-            }),
-        });
-        return ({
+const gridStyles = theme => {
+    const generateBreakpointStyle = size => ({
+        [theme.breakpoints.up(size)]: ({
+            width: props => props[size] && props[size].width,
+            height: props => props[size] && props[size].height,
+        }),
+    });
+    return ({
+        grid: ({
             ...generateBreakpointStyle('xs'),
             ...generateBreakpointStyle('sm'),
             ...generateBreakpointStyle('md'),
             ...generateBreakpointStyle('lg'),
             ...generateBreakpointStyle('xl'),
-        });
-    }
-});
+        }),
+    })
+};
 
 const useStyle = makeStyles(combineStyles(
     gridStyles,
