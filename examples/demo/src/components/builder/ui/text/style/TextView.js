2c675cc52c10469a0e3a28cc8ff483d5cc9dd05f
LuisMPS <luismario14@gmail.com>
2021-06-03 13:08:30 -0500

    Standarized material ui imports in demo

--- examples/demo/src/components/builder/ui/text/style/TextView.js
+++ examples/demo/src/components/builder/ui/text/style/TextView.js
@@ -1,4 +1,4 @@
-import {makeStyles} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
 import combineStyles from "../../../styles";
 import {viewStyles} from "../../../styles/views";
 
--- examples/demo/src/components/layers/demo/styles/LayerChild.js
+++ examples/demo/src/components/layers/demo/styles/LayerChild.js
@@ -1,4 +1,4 @@
-import {makeStyles} from "@material-ui/core";
+import {makeStyles} from "@material-ui/core/styles";
 import combineStyles from "../../../builder/styles"
 
 const childStyles = ({
