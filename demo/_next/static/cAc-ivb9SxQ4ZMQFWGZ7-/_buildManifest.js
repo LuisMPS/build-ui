515c6c2da2bb9bcb2ef977d87a50d71d3c94a357
LuisMPS <luismario14@gmail.com>
2021-06-14 15:26:02 -0500

    Deploy website - based on 8e30e00c8ae7f8ad8e0a535a5300e3ecede06a79

---
+++ demo/_next/static/cAc-ivb9SxQ4ZMQFWGZ7-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/713-634145c2de37a3b73ca5.js","static/chunks/pages/index-443594c140db6d657402.js"],"/_error":["static/chunks/pages/_error-d36babdecdd567a38448.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
